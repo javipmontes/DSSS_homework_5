28a443422ea0322a099a42ae83814f21bd27bb2c
javipmontes <javierpanimon@gmail.com>
2022-12-02 12:55:54 +0100

    setup.py completed

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from distutils.core import setup
 
+setup(name='homework 5',
+      version='1.0',
+      description='Homework 5 for Data Science Survival Skills',
+      author='Javier Paniagua Montes',
+      author_email='javier.paniagua@fau.de',
+      url='https://www.python.org/sigs/distutils-sig/',
+      install_requires=[
+        'numpy',
+        'turtle'
+    ]
+     )
