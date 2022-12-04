cc96fbbfd9ad8c4ace9dfdcf18e0c1769b1ca663
javipmontes <javierpanimon@gmail.com>
2022-12-04 13:24:48 +0100

    setup.py modified and __init__.py moved into the snowflake folder

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
-from distutils.core import setup
-
-setup(name='snowflake',
-      version='1.0',
-      description='Homework 5 for Data Science Survival Skills',
-      author='Javier Paniagua Montes',
-      author_email='javier.paniagua@fau.de',
-      install_requires=[
-        'numpy',
-        'turtles'
-    ]
-     )
+from setuptools import find_packages, setup
+setup(
+    name='snowflake',
+    packages=find_packages(include=['snowflake']),
+    version='1.0.0',
+    description='Homework 5 for Data Science Survival Skills',
+    author='Javier Paniagua Montes',
+)
