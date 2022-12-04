d49b2c4ef75b7925bb3c3c49024100fb1fbc28c2
javipmontes <javierpanimon@gmail.com>
2022-12-04 12:59:38 +0100

    setup.py name modified to snowflake

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='homework 5',
+setup(name='snowflake',
       version='1.0',
       description='Homework 5 for Data Science Survival Skills',
       author='Javier Paniagua Montes',
